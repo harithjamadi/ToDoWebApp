4c608ac809ae2d200beabedfba03ded5fb7dd61f
Harith Jamadi <114632577+harithjamadi@users.noreply.github.com>
2024-02-17 18:18:34 +0800

    Create task.js

---
+++ model/task.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    reminderDate: { type: String },
+    reminderTime: { type: String },
+    username: { type: String, required: true }
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
